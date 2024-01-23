cf87891a3425b9e0140db8da1a6f1e6791800a40
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-01-24 01:24:17 +0600

    added deployed domain

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://house-hunter.surge.sh"],
     credentials: true,
   })
 );
